a0e8817b6b8cdbd625c88040a5ab09bc2f005c93
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-12-06 00:12:58 +0600

    setup initial file folder and installation of firebase router and react bootstrap

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCeFmHMbKuv2vwVAorSeePc2tTBmWR1gQM",
+  authDomain: "react-news-site-b8c9e.firebaseapp.com",
+  projectId: "react-news-site-b8c9e",
+  storageBucket: "react-news-site-b8c9e.appspot.com",
+  messagingSenderId: "700350794401",
+  appId: "1:700350794401:web:98a9602ab88f4ea5c27ee0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
